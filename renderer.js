cde4b2323a20f817bb807ba8e47b43f48dd24b5d
fernando macias saldana <93062447+Macias131987@users.noreply.github.com>
2022-03-20 21:58:04 -0700

    Initialize change-ups

---
+++ renderer.js
@@ -0,0 +1,6 @@
+// This file is included by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off and `contextIsolation`
+// is turned on. Use `preload.js` to selectively enable
+// features needed in the renderer process.
